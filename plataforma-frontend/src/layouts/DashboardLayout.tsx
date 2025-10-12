bf44ff6e1127b653f3b1b60dcd25e605a3f28584
D4N13LCS <danielmazzini6@gmail.com>
2025-10-11 22:25:45 -0300

    Feat: Criação do Layout da área dos usuários

---
+++ plataforma-frontend/src/layouts/DashboardLayout.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen overflow-x-hidden">
+      <Sidebar />
+      <main className="flex-1 bg-gray-100 text-black p-6 overflow-y-auto">
+        {children}
+      </main>
+    </div>
+  );
+}
