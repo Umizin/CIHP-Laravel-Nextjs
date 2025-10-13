b4e7cfb7593d81931167fa7348341b65ac6f1ed9
D4N13LCS <danielmazzini6@gmail.com>
2025-10-12 21:02:53 -0300

    Fix: Ajuste na rolagem do Layout

--- plataforma-frontend/src/layouts/DashboardLayout.tsx
+++ plataforma-frontend/src/layouts/DashboardLayout.tsx
@@ -2,9 +2,12 @@ import Sidebar from "@/components/Sidebar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen overflow-x-hidden">
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar fixa */}
       <Sidebar />
-      <main className="flex-1 bg-gray-100 text-black p-6 overflow-y-auto">
+
+      {/* Conteúdo rolável */}
+      <main className="flex-1 bg-gray-100 text-black overflow-y-auto p-6">
         {children}
       </main>
     </div>
