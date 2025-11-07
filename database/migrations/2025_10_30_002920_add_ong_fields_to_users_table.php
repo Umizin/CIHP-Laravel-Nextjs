974aaa1268829308ded2a97e89e0656a4933548b
D4N13LCS <danielmazzini6@gmail.com>
2025-11-07 20:52:43 -0300

    Feat: Criação da migração da ONG

---
+++ database/migrations/2025_10_30_002920_add_ong_fields_to_users_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+        {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('ong_name')->nullable();
+                $table->string('responsavel')->nullable();
+                // $table->string('telefone')->nullable();
+                $table->text('descricao')->nullable();
+            });
+        }
+
+};
